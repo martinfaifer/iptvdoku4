6bd6483092a8859054812a43effeb69f692022eb
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2024-04-30 09:01:26 +0200

    Update settings-genius-tv-statistics-channels-component.blade.php

--- resources/views/livewire/settings/genius-tv/statistics/settings-genius-tv-statistics-channels-component.blade.php
+++ resources/views/livewire/settings/genius-tv/statistics/settings-genius-tv-statistics-channels-component.blade.php
@@ -2,7 +2,7 @@
     <div class="grid grid-cols-12 gap-4">
         {{-- all channels --}}
         <div class="col-span-12 md:col-span-6">
-            <livewire:settings.genius-tv.statistics.settings-genius-tv-statistics-for-all-channels-component lazy></livewire:settings.genius-tv.statistics.settings-genius-tv-statistics-for-all-channels-component>
+            {{-- <livewire:settings.genius-tv.statistics.settings-genius-tv-statistics-for-all-channels-component lazy></livewire:settings.genius-tv.statistics.settings-genius-tv-statistics-for-all-channels-component> --}}
         </div>
         {{-- channels per isp with dropdown menu? --}}
         <div class="col-span-12 md:col-span-6">
