5b5d92552365ec28a820186e4d38f69337b580d9
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2024-05-03 11:46:09 +0200

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -37,7 +37,7 @@
 
 {{-- bg-gradient-to-b from-[#020313] --}}
 
-<body class="bg-gradient-to-r from-slate-900 to-sky-950">
+<body class="bg-gradient-to-r from-slate-900 to-sky-950 overflow-x-hidden">
     <x-toast />
     @auth
         <x-spotlight search-text="Vyhledejte ... " no-results-text="Ops! Nenalezeno." class="justify-center"
