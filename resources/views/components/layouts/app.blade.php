ba10c69d5a419da8b982c966c29567bfb069aff3
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2024-05-21 08:28:17 +0200

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -48,106 +48,108 @@
 
         <x-main full-width class="no-scrollbar">
             @auth
-                <x-slot:sidebar drawer="main-drawer fixed"
-                    class="bg-gradient-to-b from-slate-950/80 to-black/40 border-r border-[#64748b] border-opacity-10 !w-[320px]">
-                    <x-menu activate-by-route active-bg-color="bg-sky-950" class="-ml-4 -mt-2 ">
-                        <ul class="menu bg-[#020411]/20 border-r border-[#64748b] border-opacity-10 h-full ml-2 fixed">
-                            <div class="tooltip tooltip-bottom" data-tip="Kanály">
-                                <li href="/channels" wire:navigate @class([
-                                    'rounded-lg',
-                                    'bg-[#1A1E2A]' => request()->is('channels') || request()->is('channels/*'),
-                                ])>
-                                    <a>
-                                        <x-sui-tv-mode class="size-6 text-white" fill="none" />
-                                    </a>
-                                </li>
+                @persist('sidebar')
+                    <x-slot:sidebar drawer="main-drawer fixed"
+                        class="bg-gradient-to-b from-slate-950/80 to-black/40 border-r border-[#64748b] border-opacity-10 !w-[320px]">
+                        <x-menu activate-by-route active-bg-color="bg-sky-950" class="-ml-4 -mt-2 ">
+                            <ul class="menu bg-[#020411]/20 border-r border-[#64748b] border-opacity-10 h-full ml-2 fixed">
+                                <div class="tooltip tooltip-bottom" data-tip="Kanály">
+                                    <li href="/channels" wire:navigate @class([
+                                        'rounded-lg',
+                                        'bg-[#1A1E2A]' => request()->is('channels') || request()->is('channels/*'),
+                                    ])>
+                                        <a>
+                                            <x-sui-tv-mode class="size-6 text-white" fill="none" />
+                                        </a>
+                                    </li>
+                                </div>
+                                @can('show_blade_functions', App\Model\Device::class)
+                                    <livewire:iptv.devices.menu.device-menu-icon-with-alert-component />
+                                @endcan
+                                @can('show_blade_functions', App\Model\SatelitCard::class)
+                                    <div class="tooltip tooltip-bottom" data-tip="Satelitní karty">
+                                        <li href="/sat-cards" wire:navigate @class([
+                                            'rounded-lg',
+                                            'bg-[#1A1E2A]' =>
+                                                request()->is('sat-cards') || request()->is('sat-cards/*'),
+                                        ])>
+                                            <a>
+                                                <x-heroicon-o-credit-card class="size-6 text-white/80" fill="none" />
+                                            </a>
+                                        </li>
+                                    </div>
+                                @endcan
+                                @can('show_blade_functions', App\Models\Event::class)
+                                    <div class="tooltip tooltip-bottom" data-tip="Kalendář">
+                                        <li href="/calendar" wire:navigate @class([
+                                            'rounded-lg',
+                                            'bg-[#1A1E2A]' => request()->is('calendar') || request()->is('calendar/*'),
+                                        ])>
+                                            <a>
+                                                <x-heroicon-o-calendar-days class="size-6 text-white/80" fill="none" />
+                                            </a>
+                                        </li>
+                                    </div>
+                                @endcan
+                                @can('show_servers', App\Models\SftpServer::class)
+                                    <div class="tooltip tooltip-bottom" data-tip="Sftp servery">
+                                        <li href="/sftps" wire:navigate @class([
+                                            'rounded-lg',
+                                            'bg-[#1A1E2A]' => request()->is('sftps') || request()->is('sftps/*'),
+                                        ])>
+                                            <a>
+                                                <x-heroicon-o-arrow-up-on-square-stack class="size-6 text-white/80"
+                                                    fill="none" />
+                                            </a>
+                                        </li>
+                                    </div>
+                                @endcan
+                                <div class="tooltip tooltip-bottom" data-tip="Wiki">
+                                    <li href="/wiki" wire:navigate @class([
+                                        'rounded-lg',
+                                        'bg-[#1A1E2A]' => request()->is('wiki') || request()->is('wiki/*'),
+                                    ])>
+                                        <a>
+                                            <x-heroicon-o-academic-cap class="size-6 text-white/80" fill="none" />
+                                        </a>
+                                    </li>
+                                </div>
+                                @can('show_tickets', App\Models\User::class)
+                                    <livewire:iptv.flow-eye.menu.flow-eye-menu-icon-with-alert-component />
+                                @endcan
+                            </ul>
+                        </x-menu>
+                        {{-- main dynamic navigation --}}
+                        <x-menu activate-by-route active-bg-color="bg-sky-950" class="ml-16 fixed !h-[99%]">
+                            <div class="overflow-y-scroll hover:overflow-scroll">
+                                @if (request()->is('/') || request()->is('channels') || request()->is('channels/*'))
+                                    <livewire:iptv.channels.menu.channels-menu />
+                                @endif
+                                @if (request()->is('devices') || request()->is('devices/*'))
+                                    <livewire:iptv.devices.menu.devices-menu class="fixed" />
+                                @endif
+                                @if (request()->is('sat-cards') || request()->is('sat-cards/*'))
+                                    <livewire:iptv.cards.menu.satelit-cards-menu class="fixed" />
+                                @endif
+                                @if (request()->is('sftps') || request()->is('sftps/*'))
+                                    <livewire:iptv.sftps.menu.sftps-menu class="fixed" />
+                                @endif
+                                @if (request()->is('wiki') || request()->is('wiki/*'))
+                                    <livewire:wiki.menu.wiki-menu-component class="fixed" />
+                                @endif
+                                @if (request()->is('floweye') || request()->is('floweye/*'))
+                                    <livewire:iptv.flow-eye.menu.flow-eye-menu-component class="fixed" />
+                                @endif
+                                @if (request()->is('profile') || request()->is('profile/*'))
+                                    <livewire:user.menu.user-menu-component class="fixed" />
+                                @endif
+                                @if (request()->is('settings') || request()->is('settings/*'))
+                                    <livewire:settings.settings-navigation-component class="fixed" />
+                                @endif
                             </div>
-                            @can('show_blade_functions', App\Model\Device::class)
-                                <livewire:iptv.devices.menu.device-menu-icon-with-alert-component />
-                            @endcan
-                            @can('show_blade_functions', App\Model\SatelitCard::class)
-                                <div class="tooltip tooltip-bottom" data-tip="Satelitní karty">
-                                    <li href="/sat-cards" wire:navigate @class([
-                                        'rounded-lg',
-                                        'bg-[#1A1E2A]' =>
-                                            request()->is('sat-cards') || request()->is('sat-cards/*'),
-                                    ])>
-                                        <a>
-                                            <x-heroicon-o-credit-card class="size-6 text-white/80" fill="none" />
-                                        </a>
-                                    </li>
-                                </div>
-                            @endcan
-                            @can('show_blade_functions', App\Models\Event::class)
-                                <div class="tooltip tooltip-bottom" data-tip="Kalendář">
-                                    <li href="/calendar" wire:navigate @class([
-                                        'rounded-lg',
-                                        'bg-[#1A1E2A]' => request()->is('calendar') || request()->is('calendar/*'),
-                                    ])>
-                                        <a>
-                                            <x-heroicon-o-calendar-days class="size-6 text-white/80" fill="none" />
-                                        </a>
-                                    </li>
-                                </div>
-                            @endcan
-                            @can('show_servers', App\Models\SftpServer::class)
-                                <div class="tooltip tooltip-bottom" data-tip="Sftp servery">
-                                    <li href="/sftps" wire:navigate @class([
-                                        'rounded-lg',
-                                        'bg-[#1A1E2A]' => request()->is('sftps') || request()->is('sftps/*'),
-                                    ])>
-                                        <a>
-                                            <x-heroicon-o-arrow-up-on-square-stack class="size-6 text-white/80"
-                                                fill="none" />
-                                        </a>
-                                    </li>
-                                </div>
-                            @endcan
-                            <div class="tooltip tooltip-bottom" data-tip="Wiki">
-                                <li href="/wiki" wire:navigate @class([
-                                    'rounded-lg',
-                                    'bg-[#1A1E2A]' => request()->is('wiki') || request()->is('wiki/*'),
-                                ])>
-                                    <a>
-                                        <x-heroicon-o-academic-cap class="size-6 text-white/80" fill="none" />
-                                    </a>
-                                </li>
-                            </div>
-                            @can('show_tickets', App\Models\User::class)
-                                <livewire:iptv.flow-eye.menu.flow-eye-menu-icon-with-alert-component />
-                            @endcan
-                        </ul>
-                    </x-menu>
-                    {{-- main dynamic navigation --}}
-                    <x-menu activate-by-route active-bg-color="bg-sky-950" class="ml-16 fixed !h-[99%]">
-                        <div class="overflow-y-scroll hover:overflow-scroll">
-                            @if (request()->is('/') || request()->is('channels') || request()->is('channels/*'))
-                                <livewire:iptv.channels.menu.channels-menu />
-                            @endif
-                            @if (request()->is('devices') || request()->is('devices/*'))
-                                <livewire:iptv.devices.menu.devices-menu class="fixed" />
-                            @endif
-                            @if (request()->is('sat-cards') || request()->is('sat-cards/*'))
-                                <livewire:iptv.cards.menu.satelit-cards-menu class="fixed" />
-                            @endif
-                            @if (request()->is('sftps') || request()->is('sftps/*'))
-                                <livewire:iptv.sftps.menu.sftps-menu class="fixed" />
-                            @endif
-                            @if (request()->is('wiki') || request()->is('wiki/*'))
-                                <livewire:wiki.menu.wiki-menu-component class="fixed" />
-                            @endif
-                            @if (request()->is('floweye') || request()->is('floweye/*'))
-                                <livewire:iptv.flow-eye.menu.flow-eye-menu-component class="fixed" />
-                            @endif
-                            @if (request()->is('profile') || request()->is('profile/*'))
-                                <livewire:user.menu.user-menu-component class="fixed" />
-                            @endif
-                            @if (request()->is('settings') || request()->is('settings/*'))
-                                <livewire:settings.settings-navigation-component class="fixed" />
-                            @endif
-                        </div>
-                    </x-menu>
-                </x-slot:sidebar>
+                        </x-menu>
+                    </x-slot:sidebar>
+                @endpersist
             @endauth
             <x-slot:content class="mt-14 no-scrollbar">
                 @auth
