a1f74f5e42662c7534e149cd83d6fd6c5d1e1116
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2024-06-03 09:38:19 +0200

    create command for creating pdf invoices ( emergency use if is needed )

--- app/Console/Commands/CreateNanguIspsInvoiceForThisMonthCommand.php
+++ app/Console/Commands/CreateNanguIspsInvoiceForThisMonthCommand.php
@@ -5,27 +5,27 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Services\Invoices\CreateNanguInvoicePerIsp;
 
-class TestFeatureCommand extends Command
+class CreateNanguIspsInvoiceForThisMonthCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'feature:test';
+    protected $signature = 'invoices:create-nangu-isps-for-this-month';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Testing upcoming features';
+    protected $description = 'Comamnd for create nangu isps invoice for this month, only for single uses, if anythinks breaks ';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        echo "No feature yet";
+        (new CreateNanguInvoicePerIsp())->create();
     }
 }
