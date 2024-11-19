6a34d12c8713d63609a9c41166b0c10fde38d64b
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2024-11-19 12:03:58 +0100

    Update SettingsGeniusTvStatisticsPlusOrFilmChannelPackageComponent.php

--- app/Livewire/Settings/Geniustv/Statistics/SettingsGeniusTvStatisticsPlusOrFilmChannelPackageComponent.php
+++ app/Livewire/Settings/Geniustv/Statistics/SettingsGeniusTvStatisticsPlusOrFilmChannelPackageComponent.php
@@ -10,6 +10,35 @@ use App\Exports\MaxPackageUsageExport;
 
 class SettingsGeniusTvStatisticsPlusOrFilmChannelPackageComponent extends Component
 {
+    protected function isp_qeury_data(int $ispId)
+    {
+        return match ($ispId) {
+            1 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%film%")->where('offers', "not like", "%filmbox%")->count(),
+            2 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%plus%")->where('offers', "not like", "%filmbox%")->count(),
+            3 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%film%")->where('offers', "not like", "%filmbox%")->count(),
+            4 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%film%")->where('offers', "not like", "%filmbox%")->count(),
+            5 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%film%")->where('offers', "not like", "%filmbox%")->count(),
+            6 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%film%")->where('offers', "not like", "%filmbox%")->count(),
+            7 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('tariffCode', "like", "%plus%")->count(),
+            8 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%film%")->where('offers', "not like", "%filmbox%")->count(),
+            9 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%film%")->where('offers', "not like", "%filmbox%")->count(),
+            10 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%film%")->where('offers', "not like", "%filmbox%")->count(),
+            11 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('tariffCode', "like", "%plus%")->count(),
+            12 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('tariffCode', "like", "%plus%")->count(),
+            18 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('tariffCode', "like", "%plus%")->count(),
+            19 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%film%")->where('offers', "not like", "%filmbox%")->count(),
+            20 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('tariffCode', "like", "%plus%")->count(),
+            21 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('tariffCode', "like", "%plus%")->count(),
+            22 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('tariffCode', "like", "%plus%")->count(),
+            23 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%film%")->where('offers', "not like", "%filmbox%")->count(),
+            24 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('tariffCode', "like", "%plus%")->count(),
+            25 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%film%")->where('offers', "not like", "%filmbox%")->count(),
+            26 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('offers', "like", "%film%")->where('offers', "not like", "%filmbox%")->count(),
+            27 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('tariffCode', "like", "%plus%")->count(),
+            28 => NanguSubscription::where('nangu_isp_id', $ispId)->where('subscriptionState', "!=", "SUSPENDED")->where('tariffCode', "like", "%plus%")->count(),
+            default => 0
+        };
+    }
 
     public function usage(): array
     {
@@ -19,15 +48,7 @@ class SettingsGeniusTvStatisticsPlusOrFilmChannelPackageComponent extends Compon
         foreach ($nanguIsps as $isp) {
             $result[] = [
                 'isp' => $isp->name,
-                'usage' => NanguSubscription
-                    ::where('nangu_isp_id', $isp->id)
-                    ->where('subscriptionState', "!=", "SUSPENDED")
-                    ->where('offers', "like", "%PLUS%")
-                    ->count() + NanguSubscription
-                    ::where('nangu_isp_id', $isp->id)
-                    ->where('subscriptionState', "!=", "SUSPENDED")
-                    ->Where('offers', "like", "%FILM%")
-                    ->count()
+                'usage' => $this->isp_qeury_data($isp->id)
             ];
         }
 
