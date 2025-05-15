d393fcb4b33cccc3ce06a6a9cd71f0838c3ae2ee
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2025-05-15 10:10:56 +0200

    Update base-card.blade.php

--- resources/views/components/share/cards/base-card.blade.php
+++ resources/views/components/share/cards/base-card.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="bg-[#F6F7F9] dark:bg-[#0f172a]/50 border-[2px] border-slate-200 dark:border-none backdrop-blur-xl shadow-sm dark:shadow-md dark:shadow-[#0D243C]/50 rounded-lg">
+    <div class="bg-[#F6F7F9] dark:bg-[#0f172a]/50 border-[2px] border-slate-200/80 dark:border-none backdrop-blur-xl shadow-sm dark:shadow-md dark:shadow-[#0D243C]/50 rounded-lg">
         <div class="pt-2">
             <p class="text-center text-sm font-semibold">
                 {{ $title }}
